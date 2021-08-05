28731e5bbcbd095dd7f56c54dba1963c642ffaa7
eds2109 <warlock609@gmail.com>
2021-08-05 17:39:55 +0300

    add scss code header

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -8,6 +8,15 @@ window.onload = function () {
       if (targetElement.classList.contains('menu__arrow')) {
         targetElement.closest('.menu__item').classList.toggle('_hover')
       }
+      if (
+        !targetElement.closest('.menu__item') &&
+        document.querySelectorAll('.menu__item._hover').length > 0
+      ) {
+        _removeClasses(
+          document.querySelectorAll('.menu__item._hover'),
+          '_hover',
+        )
+      }
     }
   }
 }
